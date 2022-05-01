474461e7d5f8639237e120c4e0199b95f2bc2dda
hyy <2896306235@qq.com>
2022-05-01 19:22:32 +0800

    web 项目启动

---
+++ xx-web/src/main/java/com/xx/web/XxWebApplication.java
@@ -0,0 +1,11 @@
+package com.xx.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XxWebApplication.class);
+    }
+}

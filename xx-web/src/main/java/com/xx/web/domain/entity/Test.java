74a8f6d63f5b6afa2c89a90e4def6107e7d348c0
hanyangyang <2896306235@qq.com>
2022-05-05 10:34:12 +0800

    jap的引入

--- xx-web/src/main/java/com/xx/web/domain/entity/Test.java
+++ xx-web/src/main/java/com/xx/web/domain/entity/Test.java
@@ -5,10 +5,17 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 @TableName("app_test")
 public class Test {
+    @Id
     @TableId(type = IdType.AUTO)
     private Long id;
+
+    /**内容*/
+    @Column(name = "content1", length = 2, nullable = false, columnDefinition = "comment '测试内容'")
     private String content;
 }

f2a5325a6db363edb3c898ad7e6c161313b7e0b0
hanyangyang <2896306235@qq.com>
2022-05-05 17:51:17 +0800

    测试报错还没有处理好

--- xx-web/src/main/java/com/xx/web/mapper/TestMapper.java
+++ xx-web/src/main/java/com/xx/web/mapper/TestMapper.java
@@ -2,6 +2,8 @@ package com.xx.web.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xx.web.domain.entity.Test;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface TestMapper extends BaseMapper<Test> {
 }
--- xx-web/src/test/java/com/xx/web/test/MyBatisPlusTest.java
+++ xx-web/src/test/java/com/xx/web/test/MyBatisPlusTest.java
@@ -1,17 +1,18 @@
 package com.xx.web.test;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.xx.web.XxWebApplication;
 import com.xx.web.mapper.TestMapper;
 import lombok.extern.slf4j.Slf4j;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 import java.util.List;
 
 @Slf4j
-@SpringBootTest(classes = XxWebApplication.class)
+@SpringBootTest()
 public class MyBatisPlusTest {
 
     @Autowired
@@ -19,7 +20,12 @@ public class MyBatisPlusTest {
 
     @Test
     public void testSelect(){
-        List<com.xx.web.domain.entity.Test> list = testMapper.selectList(new LambdaQueryWrapper<>());
-        log.info("查询结果：{}",list);
+        try {
+            List<com.xx.web.domain.entity.Test> list = testMapper.selectList(new LambdaQueryWrapper<>());
+            log.info("查询结果：{}",list);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }

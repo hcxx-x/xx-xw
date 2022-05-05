a352f20ed8f4c7674cf3e02ddd92655f4cd221d4
hyy <2896306235@qq.com>
2022-05-05 21:26:48 +0800

    jpa可以了

--- xx-web/src/main/java/com/xx/web/domain/entity/TestJpa.java
+++ xx-web/src/main/java/com/xx/web/domain/entity/TestJpa.java
@@ -2,16 +2,20 @@ package com.xx.web.domain.entity;
 
 
 import lombok.Data;
+import org.hibernate.annotations.Comment;
 
 import javax.persistence.*;
 
 @Data
 @Entity(name = "test_jpa")
+@Table(name = "test_jpa")
 public class TestJpa {
     @Id
+    @Column(name = "id")
     private Long id;
 
     /**内容*/
-    @Column(name = "content", length = 2, nullable = false, columnDefinition = "comment '测试内容'")
+    @Column(name = "content", length = 2, nullable = false)
+    @Comment("测试内容")
     private String content;
 }

37c10472bd85946b1048d7c3ee6ee5fb676d244a
hanyangyang <2896306235@qq.com>
2022-05-05 15:45:14 +0800

    actable 逆向生成表

--- xx-web/src/main/java/com/xx/web/XxWebApplication.java
+++ xx-web/src/main/java/com/xx/web/XxWebApplication.java
@@ -3,11 +3,21 @@ package com.xx.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@MapperScan(basePackages = "com.xx.web.mapper")
+/**
+ *  com.gitee.sunchenbin.mybatis.actable.dao.*  和  com.gitee.sunchenbin.mybatis.actable.manager.* 的相关配置是使用acTable 逆向生成表所需要的
+ *  如果不需要可以将相关配置去掉
+ */
+@SpringBootApplication(scanBasePackages = {"com.gitee.sunchenbin.mybatis.actable.manager.*"})
+@MapperScan(basePackages = {"com.gitee.sunchenbin.mybatis.actable.dao"})
 public class XxWebApplication {
     public static void main(String[] args) {
-        SpringApplication.run(XxWebApplication.class);
+        try {
+            SpringApplication.run(XxWebApplication.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }

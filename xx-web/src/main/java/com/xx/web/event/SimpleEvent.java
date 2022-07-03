c4ea8c34091be9c88cbad6d22167ee42ce2d48a1
2896306235@qq.com <2896306235@qq.com>
2022-07-03 19:45:11 +0800

    改造成gradle

--- xx-web/src/main/java/com/xx/web/event/SimpleEvent.java
+++ xx-web/src/main/java/com/xx/web/event/SimpleEvent.java
@@ -1,12 +1,10 @@
 package com.xx.web.event;
 
-import lombok.Data;
 import org.springframework.context.ApplicationEvent;
 
 /**
  * @author hanyangyang
  */
-@Data
 public class SimpleEvent extends ApplicationEvent {
     public SimpleEvent(Object source) {
         super(source);

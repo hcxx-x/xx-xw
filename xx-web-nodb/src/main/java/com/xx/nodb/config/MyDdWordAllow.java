43c72451990e1d00eae7f015cb2a9ef305c4a43c
hanyangyang <2896306235@qq.com>
2022-10-11 16:31:40 +0800

    敏感词过滤

--- xx-web-nodb/src/main/java/com/xx/nodb/config/MyDdWordAllow.java
+++ xx-web-nodb/src/main/java/com/xx/nodb/config/MyDdWordAllow.java
@@ -2,7 +2,10 @@ package com.xx.nodb.config;
 
 import com.github.houbb.sensitive.word.api.IWordAllow;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import javax.lang.model.element.VariableElement;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +19,19 @@ import java.util.stream.Stream;
 public class MyDdWordAllow implements IWordAllow {
     @Override
     public List<String> allow() {
-        return Stream.of("性", "干", "屏蔽").collect(Collectors.toList());
+        ArrayList<String> allowList = new ArrayList<>();
+        try (   FileInputStream fileInputStream = new FileInputStream("build/resources/main/sensitive_words/allow.txt");
+                InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader)
+        ) {
+           String context = bufferedReader.readLine();
+            while (StringUtils.hasLength(context)){
+                allowList.add(context);
+                context = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return allowList;
     }
 }

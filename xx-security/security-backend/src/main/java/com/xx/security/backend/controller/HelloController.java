94da49b227761945b796d05ca5a2f9ea5604bcc2
2896306235@qq.com <2896306235@qq.com>
2022-11-05 23:00:54 +0800

    springsecurity 学习项目初始化

---
+++ xx-security/security-backend/src/main/java/com/xx/security/backend/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.xx.security.backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @GetMapping("security")
+    public String helloSecurity(){
+        return "hello security";
+    }
+}

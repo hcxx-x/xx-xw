f6708c43b042bf3045a326f240f0da5293163795
hanyangyang <2896306235@qq.com>
2022-11-16 20:48:37 +0800

    spring security oath2 和 github 基本框架的搭建

---
+++ xx-security/security-oauth2-github/src/main/java/com/security/oauth2/github/SecurityOauth2GithubApp.java
@@ -0,0 +1,15 @@
+package com.security.oauth2.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: hanyangyang
+ * @date: 2022/11/16
+ */
+@SpringBootApplication
+public class SecurityOauth2GithubApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOauth2GithubApp.class,args);
+    }
+}

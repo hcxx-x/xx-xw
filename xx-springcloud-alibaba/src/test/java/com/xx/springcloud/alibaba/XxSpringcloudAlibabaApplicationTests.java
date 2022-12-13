a4428a64c55413eea86eba7d1a60cabf41925b7e
2896306235@qq.com <2896306235@qq.com>
2022-12-13 20:34:27 +0800

    juc 相关测试代码

---
+++ xx-springcloud-alibaba/src/test/java/com/xx/springcloud/alibaba/XxSpringcloudAlibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xx.springcloud.alibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxSpringcloudAlibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

73e57dd43495bce5b030de6ed72250b8f028768b
hanyangyang <2896306235@qq.com>
2023-03-27 17:42:18 +0800

    创建一个关于日志输出的项目，并整合H2数据库

---
+++ xx-springboot-log/src/main/java/com/xx/log/XxLogApplication.java
@@ -0,0 +1,12 @@
+package com.xx.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxLogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XxLogApplication.class, args);
+    }
+
+}
---
+++ xx-springboot-log/src/test/java/com/xx/log/XxSpringbootLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xx.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxSpringbootLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

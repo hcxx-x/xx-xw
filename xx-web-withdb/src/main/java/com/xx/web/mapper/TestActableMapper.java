d8c20f880b355507ad0fcd9670ef59208ed98407
hanyangyang <2896306235@qq.com>
2023-05-17 10:27:44 +0800

    springboot3 分支初始化

--- xx-web-withdb/src/main/java/com/xx/web/mapper/TestActableMapper.java
+++
@@ -1,7 +0,0 @@
-package com.xx.web.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xx.web.domain.entity.TestActable;
-
-public interface TestActableMapper extends BaseMapper<TestActable> {
-}

e7e4f5dae5446426b205d538daa3f74d65968f47
hanyangyang <2896306235@qq.com>
2023-07-30 11:06:55 +0800

    sharding-jdbc 垂直分库

---
+++ sharding-sphere-vertical-split-db/src/test/java/shardingsphereverticalsplitdb/ShardingSphereVerticalSplitDbApplication.java
@@ -0,0 +1,10 @@
+package shardingsphereverticalsplitdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ShardingSphereVerticalSplitDbApplication {
+
+}

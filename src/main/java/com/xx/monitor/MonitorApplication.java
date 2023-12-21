1a08f7ed3040fa9d82ca68799e39679c4e647477
hanyangyang <2896306235@qq.com>
2023-12-21 15:39:59 +0800

    prometheus-grafana工程初始化

---
+++ src/main/java/com/xx/monitor/MonitorApplication.java
@@ -0,0 +1,15 @@
+package com.xx.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hanyangyang
+ * @since 2023/12/8
+ */
+@SpringBootApplication
+public class MonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class,args);
+    }
+}

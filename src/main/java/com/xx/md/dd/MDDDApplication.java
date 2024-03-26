421360dec7057a1c4550ab22ce890741c940383e
hanyangyang <2896306235@qq.com>
2024-03-26 18:00:33 +0800

    初始化启动类

--- src/main/java/com/xx/md/dd/MDDDApplication.java
+++ src/main/java/com/xx/md/dd/MDDDApplication.java
@@ -1,8 +1,15 @@
 package com.xx.md.dd;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author hanyangyang
  * @since 2024/3/26
  */
+@SpringBootApplication
 public class MDDDApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MDDDApplication.class, args);
+    }
 }

749d4e6f14e9d1d99c2aa6b5e0328aa65984850e
hanyangyang <2896306235@qq.com>
2024-04-16 17:52:00 +0800

    feign 拦截响应

--- xw-service1/src/main/java/com/xx/so/feign/ResponseDecoder.java
+++ xw-service1/src/main/java/com/xx/so/feign/ResponseDecoder.java
@@ -1,6 +1,7 @@
 package com.xx.so.feign;
 
 
+import cn.hutool.json.JSONConfig;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -14,12 +15,15 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.cloud.openfeign.support.SpringDecoder;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -73,11 +77,29 @@ public class ResponseDecoder implements Decoder {
         // TODO 可以根据注解判断是否需要执行下面的操作
         String bodyStr = Util.toString(response.body().asReader(Util.UTF_8));
         JSONObject jsonObject = JSONUtil.parseObj(bodyStr);
-        Integer code = jsonObject.getInt("code");
-        if (Objects.equals(code, 0)){
-            return jsonObject.getObj("data");
+        // 拿到Json对象最顶层的key
+        Set<String> jsonKey = jsonObject.keySet();
+        Field[] declaredFields = HttpR.class.getDeclaredFields();
+        Set<String> fieldNameSet = Arrays.stream(declaredFields).map(Field::getName).collect(Collectors.toSet());
+        // 如果属性名和json key完全匹配，说明符合HttpR的数据结构
+        if (fieldNameSet.containsAll(jsonKey) && jsonKey.containsAll(fieldNameSet)){
+            // 说明符合HttpR的数据结构
+            Integer code = jsonObject.getInt("code");
+            if (Objects.equals(code, 0)){
+                return jsonObject.getObj("data");
+            }else{
+                // 可以抛出自定义异常或者解码异常等，根据需要定义即可
+                throw new RuntimeException(jsonObject.getStr("msg"));
+            }
         }else{
-            throw new RuntimeException(jsonObject.getStr("msg"));
+            return this.decoder.decode(response, type);
         }
+
+
+    }
+
+    public static void main(String[] args) {
+        JSONObject jsonObject = JSONUtil.parseObj("{\"code1\":0,\"msg\":\"\",\"data\":{\"id\":1,\"name\":\"张三\"}}");
+        System.out.println(jsonObject.keySet());
     }
 }

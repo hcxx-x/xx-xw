abd942948097b32f5e0afbbee4bde0c9b4d2dbaa
hanyangyang <2896306235@qq.com>
2024-07-02 11:40:44 +0800

    修改springboot 插件版本号

--- springboot-web-config/src/main/java/com/xx/webconfig/WebConfigApplication.java
+++ springboot-web-config/src/main/java/com/xx/webconfig/WebConfigApplication.java
@@ -1,11 +1,13 @@
 package com.xx.webconfig;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author hanyangyang
  * @since 2024/7/1
  */
+@SpringBootApplication
 public class WebConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebConfigApplication.class,args);

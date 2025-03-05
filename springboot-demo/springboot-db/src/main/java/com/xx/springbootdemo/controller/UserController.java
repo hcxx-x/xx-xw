66c206c501e279970330dfdf70b7e6557ed8587d
hanyangyang <2896306235@qq.com>
2025-03-05 17:31:42 +0800

    验证springboot能用Long类型接收前端入参的数字字符串

--- springboot-demo/springboot-db/src/main/java/com/xx/springbootdemo/controller/UserController.java
+++ springboot-demo/springboot-db/src/main/java/com/xx/springbootdemo/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.xx.springbootdemo.controller;
 
+import com.xx.springbootdemo.entity.User;
 import com.xx.springbootdemo.learn.TransactionWithLockLearn;
 import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +25,12 @@ public class UserController {
   public String repeatInsertUser(@RequestParam("phone") String phone) throws InterruptedException {
     transactionWithLockLearn.TransactionDefinitionTx(phone);
     return "SUCCESS";
+  }
+
+
+  @PostMapping("/typeConvert")
+  public String validTypeConvert(@RequestBody User user){
+    System.out.println(Long.MAX_VALUE);
+    return user.toString();
   }
 }
